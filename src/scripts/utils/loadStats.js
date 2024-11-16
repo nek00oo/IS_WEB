04bfcb5d303dc8a8095841460f3439a8a4dfb229
nek00oo <yakovenkovalera2017@yandex.ru>
2024-11-16 14:09:46 +0300

    implemented constructor

--- src/scripts/utils/loadStats.js
+++ src/scripts/utils/loadStats.js
@@ -1,12 +1,10 @@
 (function () {
     window.addEventListener('load', function () {
         const timing = window.performance.timing;
-        const loadTime = timing.loadEventEnd - timing.navigationStart;
         const domContentLoadedTime = timing.domContentLoadedEventEnd - timing.navigationStart;
 
         const statsText = `
-            Время полной загрузки страницы: ${loadTime} мс <br>
-            Время до DOMContentLoaded: ${domContentLoadedTime} мс
+            Время загрузки: ${domContentLoadedTime} мс
         `;
 
         const statsContainer = document.getElementById('loadStats');
