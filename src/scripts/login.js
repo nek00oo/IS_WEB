d02f2294929f41e5d4c62e4574bf9b6e821906be
nek00oo <yakovenkovalera2017@yandex.ru>
2025-02-15 20:00:12 +0300

    integrated new pages

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,3 +1,16 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("sign-up");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+    container.classList.add("active");
+    console.log("click");
+})
+
+loginBtn.addEventListener("click", () => {
+    container.classList.remove("active");
+})
+
 document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
